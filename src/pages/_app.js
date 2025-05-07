1d0076a6fbcc498baa0a0b76c02f04dc596163be
Neto Russo <netoacrn1@gmail.com>
2025-05-06 23:31:56 -0300

    Configurações iniciais do projeto

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
 import "@/styles/globals.css";
-
+import { WindowWidthProvider } from "@/context/WindowWidthContext"; 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <WindowWidthProvider>
+      <Component {...pageProps} />
+    </WindowWidthProvider>
+  );
 }
