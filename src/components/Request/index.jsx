f30d905f58db8c06eefbc093baff0e7aab0a251d
Neto Russo <netoacrn1@gmail.com>
2025-05-11 02:42:15 -0300

    feat: Finalizando a section Request

--- src/components/Request/index.jsx
+++ src/components/Request/index.jsx
@@ -5,7 +5,23 @@ const Request = () => {
 
   return (
     <section className={styles.container} aria-label="Request">
-
+      <div className={styles.content}>
+        <h2 className={styles.title}>
+          Ready for your next project?
+        </h2>
+        <p className={styles.paragraph}>
+          Sit elit feugiat turpis sed integer integer accumsan turpis.
+        </p>
+        <form className={styles.form}>
+          <label htmlFor="email" className={styles.label}>Email</label>
+          <input type="email" id="email" name="email" placeholder="Enter your email" required className={styles.input} />
+          <label htmlFor="message" className={styles.label}>Message</label>
+          <textarea id="message" name="message" placeholder="What are you say ?" required className={styles.textarea} />
+          <button type="submit" className={styles.btn}>
+            Request Demo
+          </button>
+        </form>
+      </div>
     </section>
   )
 
